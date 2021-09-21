d749002d9ed3b2077d4b6ae9634517a162c5b911
A123wq123 <charles.degrandpre@polymtl.ca>
2021-09-21 11:50:10 -0400

    version avant correction des exercices partie 1 du chapitre 5.

--- exercice.py
+++ exercice.py
@@ -6,29 +6,65 @@ from typing import List
 
 
 def convert_to_absolute(number: float) -> float:
-    return 0
+    if number >= 0:
+        result = number
+    elif number < 0:
+        result = number*(-1)
+    return result
 
 
 def use_prefixes() -> List[str]:
     prefixes, suffixe = 'JKLMNOPQ', 'ack'
-
-    return [""]
+    liste_nom = []
+    for letter in prefixes:
+        liste_nom.append(letter+suffixe)
+    return liste_nom
 
 
 def prime_integer_summation() -> int:
-    return 0
+    somme = 0
+    liste_premiers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
+                      101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197
+                      , 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311,
+                      313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433
+                      , 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541]
+    for nombre in liste_premiers:
+        somme +=nombre
+    return somme
 
 
 def factorial(number: int) -> int:
-    return 0
+    result = 1
+    while number > 1:
+        result = result*number
+        number -= 1
+    return result
 
 
 def use_continue() -> None:
-    pass
+    for entier in range(1, 11):
+        if entier ==5:
+            continue
+        print (entier)
 
 
 def verify_ages(groups: List[List[int]]) -> List[bool]:
-    return []
+    accepted = []
+    for position in range(len(groups)):
+        if any(element==25 for element in groups[position]):
+            accepted.append(groups[position])
+        else:
+            if len(groups[position]) >10:
+                pass
+            elif len(groups[position]) <= 3:
+                pass
+            elif any(element<18 for element in groups[position]):
+                pass
+            elif any(element>70 for element in groups[position]) and any (element==50 for element in groups[position]):
+                pass
+            else:
+                accepted.append(groups[position])
+    return accepted
 
 
 def main() -> None:
